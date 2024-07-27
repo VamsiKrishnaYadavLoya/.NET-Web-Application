5b4c12840a151fada2347388ca2b4d1614468375
VamsiKrishnaLoya <vamsi.l@w4m.ai>
2024-07-27 15:51:22 -0700

    Pushing Code to Azure Repo

---
+++ TestWebApp/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestWebApp
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ VamsiWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VamsiWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
